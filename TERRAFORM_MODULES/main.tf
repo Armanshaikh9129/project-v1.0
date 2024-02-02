3ab32d5ea79489a41ddf2dd676be009fa37157b9
Armanshaikh9129 <155525862+Armanshaikh9129@users.noreply.github.com>
2024-02-02 13:35:52 +0530

    Update main.tf

--- TERRAFORM_MODULES/main.tf
+++ TERRAFORM_MODULES/main.tf
@@ -4,13 +4,15 @@ module "ec2_instance_module" {
   m_instance_type = var.p_instance_type
   m_availability = var.p_availability
   m_ins_name = var.p_ins_name
-
+  m_SG = [var.p_security_group_name]
+  m_keyname = module.SSH_KEY.SSH
 }
 
 module "Security__group"{
  source = "./modules/SECURITY-GROUP"
  m_port = var.p_port
  m_cidr_value = var.p_cidr_value
+ m_SG = var.p_security_group_name
 }
 
 module "SSH_KEY" {
