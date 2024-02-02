68d7909cffdf235e1779a936e0f5858875ebf635
Armanshaikh9129 <155525862+Armanshaikh9129@users.noreply.github.com>
2024-02-02 13:33:17 +0530

    Update main.tf

--- TERRAFORM_MODULES/modules/EC2/main.tf
+++ TERRAFORM_MODULES/modules/EC2/main.tf
@@ -2,10 +2,13 @@ resource "aws_instance" "this_ec2_instance" {
   ami = var.m_ami
   instance_type = var.m_instance_type
   availability_zone = var.m_availability
+  security_groups = var.m_SG
+  key_name = var.m_keyname
   tags = {
     Name = var.m_ins_name
   }
 }
+
 
 
 
